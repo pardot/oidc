f20f3fe2edc2095b97909226f347f2f5447b8f43
Lincoln Stoll <lincoln.stoll@salesforce.com>
2018-09-26 12:56:18 -0500

    Rename Type - Algorithm
    More accurate reflection of name.

--- internal/webauthn/types.go
+++ internal/webauthn/types.go
@@ -215,10 +215,10 @@ func (d *AuthenticatorData) HasExtensions() bool {
 }
 
 type COSEPublicKey struct {
-	_struct bool                    `codec:",int"`
-	Type    COSEAlgorithmIdentifier `codec:"3"`
-	KeyType int8                    `codec:"1"`
-	Curve   int8                    `gorm:"not null" codec:"-1"`
-	X       []byte                  `gorm:"not null" codec:"-2"`
-	Y       []byte                  `gorm:"not null" codec:"-3"`
+	_struct   bool                    `codec:",int"`
+	Algorithm COSEAlgorithmIdentifier `codec:"3"`
+	KeyType   int8                    `codec:"1"`
+	Curve     int8                    `gorm:"not null" codec:"-1"`
+	X         []byte                  `gorm:"not null" codec:"-2"`
+	Y         []byte                  `gorm:"not null" codec:"-3"`
 }
