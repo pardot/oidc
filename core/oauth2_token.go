pardot/deci

Oauth2 server helpers
This adds a set of helper functions for handling the basic oauth2 auth and
token endpoint implementations. These do not implement the flow, but make it
easier to parse requests, send responses, and handle errors.

This has been kept private for now, as it's in flux.

core/oauth2_token.go
@@ -1,0 +1,106 @@
+package core
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type GrantType string
+
+const (
+	GrantTypeAuthorizationCode GrantType = "authorization_code"
+)
+
+type tokenRequest struct {
+	GrantType    GrantType
+	Code         string
+	RedirectURI  string
+	ClientID     string
+	ClientSecret string
+}
+
+// parseTokenRequest parses the information from a request for an access token.
+//
+// https://tools.ietf.org/html/rfc6749#section-4.1.3
+func parseTokenRequest(req *http.Request) (*tokenRequest, error) {
+	if req.Method != http.MethodPost {
+		return nil, &tokenError{code: tokenErrorCodeInvalidRequest, description: "method must be POST"}
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, &tokenError{code: tokenErrorCodeInvalidRequest, description: "failed to parse request", cause: err}
+	}
+
+	tr := &tokenRequest{
+		RedirectURI: req.FormValue("redirect_uri"),
+		Code:        req.FormValue("code"),
+	}
+
+	if tr.Code == "" {
+		return nil, &tokenError{code: tokenErrorCodeInvalidRequest, description: "code is required"}
+	}
+
+	// Auth the request
+	// https://tools.ietf.org/html/rfc6749#section-2.3
+	cid, cs, isBasic := req.BasicAuth()
+	if isBasic {
+		tr.ClientID = cid
+		tr.ClientSecret = cs
+	} else {
+		tr.ClientID = req.FormValue("client_id")
+		tr.ClientSecret = req.FormValue("client_secret")
+	}
+
+	switch req.FormValue("grant_type") {
+	case string(GrantTypeAuthorizationCode):
+		tr.GrantType = GrantTypeAuthorizationCode
+	default:
+		return nil, &tokenError{code: tokenErrorCodeInvalidGrant, description: fmt.Sprintf("grant_type must be %s", GrantTypeAuthorizationCode)}
+	}
+
+	return tr, nil
+}
+
+// https://tools.ietf.org/html/rfc6749#section-5.1
+type tokenResponse struct {
+	AccessToken  string `json:"error_uri,omitempty"`
+	TokenType    string
+	ExpiresIn    time.Duration
+	RefreshToken string
+	Scopes       []string
+	ExtraParams  map[string]interface{}
+}
+
+// writeTokenResponse sends a response for the token endpoint.
+//
+// https://tools.ietf.org/html/rfc6749#section-5.1
+func writeTokenResponse(w http.ResponseWriter, resp *tokenResponse) error {
+	w.Header().Add("Content-Type", "application/json;charset=UTF-8")
+
+	respJSON := resp.ExtraParams
+	if respJSON == nil {
+		respJSON = make(map[string]interface{})
+	}
+
+	respJSON["access_token"] = resp.AccessToken
+	respJSON["token_type"] = resp.TokenType
+
+	if resp.ExpiresIn != 0 {
+		respJSON["expires_in"] = int(resp.ExpiresIn.Seconds())
+	}
+	if resp.RefreshToken != "" {
+		respJSON["refresh_token"] = resp.RefreshToken
+	}
+	if resp.Scopes != nil {
+		respJSON["scope"] = strings.Join(resp.Scopes, " ")
+	}
+
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		return fmt.Errorf("failed to write token response json body: %w", err)
+	}
+
+	return nil
+}

pardot/oidc

core/oauth2_token.go
@@ -1,0 +1,109 @@
+package core
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type GrantType string
+
+const (
+	GrantTypeAuthorizationCode GrantType = "authorization_code"
+)
+
+type tokenRequest struct {
+	GrantType    GrantType
+	Code         string
+	RedirectURI  string
+	ClientID     string
+	ClientSecret string
+}
+
+// parseTokenRequest parses the information from a request for an access token.
+//
+// https://tools.ietf.org/html/rfc6749#section-4.1.3
+func parseTokenRequest(req *http.Request) (*tokenRequest, error) {
+	if req.Method != http.MethodPost {
+		return nil, &tokenError{Code: tokenErrorCodeInvalidRequest, Description: "method must be POST"}
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, &tokenError{Code: tokenErrorCodeInvalidRequest, Description: "failed to parse request", Cause: err}
+	}
+
+	tr := &tokenRequest{
+		RedirectURI: req.FormValue("redirect_uri"),
+		Code:        req.FormValue("code"),
+	}
+
+	if tr.Code == "" {
+		return nil, &tokenError{Code: tokenErrorCodeInvalidRequest, Description: "code is required"}
+	}
+
+	// Auth the request
+	// https://tools.ietf.org/html/rfc6749#section-2.3
+	cid, cs, isBasic := req.BasicAuth()
+	if isBasic {
+		tr.ClientID = cid
+		tr.ClientSecret = cs
+	} else {
+		tr.ClientID = req.FormValue("client_id")
+		tr.ClientSecret = req.FormValue("client_secret")
+	}
+
+	switch req.FormValue("grant_type") {
+	case string(GrantTypeAuthorizationCode):
+		tr.GrantType = GrantTypeAuthorizationCode
+	default:
+		return nil, &tokenError{Code: tokenErrorCodeInvalidGrant, Description: fmt.Sprintf("grant_type must be %s", GrantTypeAuthorizationCode)}
+	}
+
+	return tr, nil
+}
+
+// https://tools.ietf.org/html/rfc6749#section-5.1
+//
+// this does eventually end up as JSON, but because of how we want to handle the
+// extra params bit we don't tag this struct
+type tokenResponse struct {
+	AccessToken  string
+	TokenType    string
+	ExpiresIn    time.Duration
+	RefreshToken string
+	Scopes       []string
+	ExtraParams  map[string]interface{}
+}
+
+// writeTokenResponse sends a response for the token endpoint.
+//
+// https://tools.ietf.org/html/rfc6749#section-5.1
+func writeTokenResponse(w http.ResponseWriter, resp *tokenResponse) error {
+	w.Header().Add("Content-Type", "application/json;charset=UTF-8")
+
+	respJSON := resp.ExtraParams
+	if respJSON == nil {
+		respJSON = make(map[string]interface{})
+	}
+
+	respJSON["access_token"] = resp.AccessToken
+	respJSON["token_type"] = resp.TokenType
+
+	if resp.ExpiresIn != 0 {
+		respJSON["expires_in"] = int(resp.ExpiresIn.Seconds())
+	}
+	if resp.RefreshToken != "" {
+		respJSON["refresh_token"] = resp.RefreshToken
+	}
+	if resp.Scopes != nil {
+		respJSON["scope"] = strings.Join(resp.Scopes, " ")
+	}
+
+	if err := json.NewEncoder(w).Encode(respJSON); err != nil {
+		return fmt.Errorf("failed to write token response json body: %w", err)
+	}
+
+	return nil
+}