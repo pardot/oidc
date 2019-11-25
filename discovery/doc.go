b4ab13db4c292a019235835ea7186d4e5f6f9a43
Lincoln Stoll <lincoln.stoll@salesforce.com>
2019-11-25 17:25:44 -0600

    Add type for OIDC discovery metadata
    This matches the JSON format served at the discovery endpoint

---
+++ discovery/doc.go
@@ -0,0 +1,5 @@
+// Package discovery implements both a server handler and client side for
+// interacting with the OIDC discovery mechanism.
+//
+// https://openid.net/specs/openid-connect-discovery-1_0.html
+package discovery
